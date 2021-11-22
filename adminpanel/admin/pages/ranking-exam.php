f80cb45531be4b931e7f88dbe65312ba911cbea9
Lucas R. Pereia <57834313+lucaskleiner@users.noreply.github.com>
2021-11-22 13:41:53 +0000

    Add files via upload

---
+++ adminpanel/admin/pages/ranking-exam.php
@@ -0,0 +1,224 @@
+<div class="app-main__outer">
+        <div class="app-main__inner">
+             
+
+
+            <?php 
+                @$exam_id = $_GET['exam_id'];
+
+
+                if($exam_id != "")
+                {
+                   $selEx = $conn->query("SELECT * FROM exam_tbl WHERE ex_id='$exam_id' ")->fetch(PDO::FETCH_ASSOC);
+                   $exam_course = $selEx['cou_id'];
+
+                   
+
+                   $selExmne = $conn->query("SELECT * FROM examinee_tbl et  WHERE exmne_course='$exam_course'  ");
+
+
+                   ?>
+                   <div class="app-page-title">
+                    <div class="page-title-wrapper">
+                        <div class="page-title-heading">
+                            <div><b class="text-primary">Status da analise</b><br>
+                                <?php echo utf8_encode('Titulo da análise'); ?> : <?php echo $selEx['ex_title']; ?><br><br>
+                               <span class="border" style="padding:10px;color:white;background-color: green;">Aprovado</span>
+                               <span class="border" style="padding:10px;color:white;background-color: blue;">Razoavel</span>
+                               <span class="border" style="padding:10px;color:white;background-color: red;">Reprovado</span>
+                               <span class="border" style="padding:10px;color:black;background-color: #E9ECEE;"><?php echo utf8_encode('Não respondeu'); ?></span>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="align-middle mb-0 table table-borderless table-striped table-hover" id="tableList">
+                          <tbody>
+                            <thead>
+                                <tr>
+                                    <th width="25%">Nome</th>
+                                    <th>Respostas favoraveis</th>
+                                    <th>Porcentagem</th>
+                                </tr>
+                            </thead>
+                            <?php 
+                                while ($selExmneRow = $selExmne->fetch(PDO::FETCH_ASSOC)) { ?>
+                                    <?php 
+                                            $exmneId = $selExmneRow['exmne_id'];
+                                            $selScore = $conn->query("SELECT * FROM exam_question_tbl eqt INNER JOIN exam_answers ea ON eqt.eqt_id = ea.quest_id AND eqt.exam_answer = ea.exans_answer  WHERE ea.axmne_id='$exmneId' AND ea.exam_id='$exam_id' AND ea.exans_status='new' ORDER BY ea.exans_id DESC");
+
+                                              $selAttempt = $conn->query("SELECT * FROM exam_attempt WHERE exmne_id='$exmneId' AND exam_id='$exam_id' ");
+
+                                             $over = $selEx['ex_questlimit_display']  ;    
+
+
+                                              @$score = $selScore->rowCount();
+                                                @$ans = $score / $over * 100;
+
+                                         ?>
+                                       <tr style="<?php 
+                                             if($selAttempt->rowCount() == 0)
+                                             {
+                                                echo "background-color: #E9ECEE;color:black";
+                                             }
+                                             else if($ans >= 90)
+                                             {
+                                                echo "background-color: yellow;";
+                                             } 
+                                             else if($ans >= 80){
+                                                echo "background-color: green;color:white";
+                                             }
+                                             else if($ans >= 75){
+                                                echo "background-color: blue;color:white";
+                                             }
+                                             else
+                                             {
+                                                echo "background-color: red;color:white";
+                                             }
+                                           
+                                            
+                                             ?>"
+                                        >
+                                        <td>
+
+                                          <?php echo $selExmneRow['exmne_fullname']; ?></td>
+                                        
+                                        <td >
+                                        <?php 
+                                          if($selAttempt->rowCount() == 0)
+                                          {
+                                            echo "Ainda nao respondeu";
+                                          }
+                                          else if($selScore->rowCount() > 0)
+                                          {
+                                            echo $totScore =  $selScore->rowCount();
+                                            echo " / ";
+                                            echo $over;
+                                          }
+                                          else
+                                          {
+                                            echo $totScore =  $selScore->rowCount();
+                                            echo " / ";
+                                            echo $over;
+                                          }
+
+                                            
+                                            
+
+                                         ?>
+                                        </td>
+                                        <td>
+                                          <?php 
+                                                if($selAttempt->rowCount() == 0)
+                                                {
+                                                  echo "Ainda nao respondeu";
+                                                }
+                                                else
+                                                {
+                                                    echo number_format($ans,2); ?>%<?php
+                                                }
+                                           
+                                          ?>
+                                        </td>
+                                    </tr>
+                                <?php }
+                             ?>                              
+                          </tbody>
+                        </table>
+                    </div>
+
+
+
+                   <?php
+                }
+                else
+                { ?>
+                <div class="app-page-title">
+                <div class="page-title-wrapper">
+                    <div class="page-title-heading">
+                        <div><b>Status de elegibilidade</b></div>
+                    </div>
+                </div>
+                </div> 
+
+                 <div class="col-md-12">
+                <div class="main-card mb-3 card">
+                    <div class="card-header"><?php echo utf8_encode('Lista de Análises'); ?>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="align-middle mb-0 table table-borderless table-striped table-hover" id="tableList">
+                            <thead>
+                            <tr>
+                                <th class="text-left pl-4"><?php echo utf8_encode('Titulo'); ?></th>
+                                <th class="text-left ">Problema</th>
+                                <th class="text-left "><?php echo utf8_encode('Introdução'); ?></th>
+                                <th class="text-center" width="8%">*****</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                              <?php 
+                                $selExam = $conn->query("SELECT * FROM exam_tbl ORDER BY ex_id DESC ");
+                                if($selExam->rowCount() > 0)
+                                {
+                                    while ($selExamRow = $selExam->fetch(PDO::FETCH_ASSOC)) { ?>
+                                        <tr>
+                                            <td class="pl-4"><?php echo $selExamRow['ex_title']; ?></td>
+                                            <td>
+                                                <?php 
+                                                    $courseId =  $selExamRow['cou_id']; 
+                                                    $selCourse = $conn->query("SELECT * FROM course_tbl WHERE cou_id='$courseId' ");
+                                                    while ($selCourseRow = $selCourse->fetch(PDO::FETCH_ASSOC)) {
+                                                        echo $selCourseRow['cou_name'];
+                                                    }
+                                                ?>
+                                            </td>
+                                            <td><?php echo $selExamRow['ex_description']; ?></td>
+                                            <td class="text-center">
+                                             <a href="?page=ranking-exam&exam_id=<?php echo $selExamRow['ex_id']; ?>"  class="btn btn-success btn-sm">View</a>
+                                            </td>
+                                        </tr>
+
+                                    <?php }
+                                }
+                                else
+                                { ?>
+                                    <tr>
+                                      <td colspan="5">
+                                        <h3 class="p-3">No Exam Found</h3>
+                                      </td>
+                                    </tr>
+                                <?php }
+                               ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>   
+                    
+                <?php }
+
+             ?>      
+            
+            
+      
+        
+</div>
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
